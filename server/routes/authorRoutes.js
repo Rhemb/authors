c24330fc03d0e74712379d53a96a4e344d9aa2dd
Rheanne Bande <rheanne.bande@gmail.com>
2023-07-22 23:34:28 -0700

    Add controller

---
+++ server/routes/authorRoutes.js
@@ -0,0 +1,27 @@
+const Author = require('../models/Author');
+
+module.exports = {
+    findAllAuthors : (req, res) => {
+        Author.find()
+            .then(allAuthors => res.json(allAuthors))
+            .catch( err => res.status(400).json(err));
+    },
+
+    createAuthor: (req, res) => {
+        Author.create(req.body)
+            .then( newAuthor => res.json(newAuthor))
+            .catch( err => res.status(400).json(err));
+    },
+
+    updateAuthor: (req, res) => {
+        Author.findByIdAndUpdate(req.params.id, req.body, {new: true})
+            .then(updatedAuthor => res.json(updatedAuthor))
+            .catch( err => res.status(400).json(err));
+    },
+
+    deleteAuthor : (req, res) => {
+        Author.findByIdAndDelete(req.params.id)
+            .then(deletedAuthor => res.json(deletedAuthor))
+            .catch( err => res.status(400).json(err));
+    }
+}
