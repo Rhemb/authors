1e3f6e96eb6a5163944c459f899b4e738b1bdaae
Rheanne Bande <rheanne.bande@gmail.com>
2023-07-22 23:38:32 -0700

    Add route and create routes

--- server/routes/authorRoutes.js
+++ server/routes/authorRoutes.js
@@ -1,27 +1,8 @@
-const Author = require('../models/Author');
+const authorController = require("../controllers/authorController");
 
-module.exports = {
-    findAllAuthors : (req, res) => {
-        Author.find()
-            .then(allAuthors => res.json(allAuthors))
-            .catch( err => res.status(400).json(err));
-    },
-
-    createAuthor: (req, res) => {
-        Author.create(req.body)
-            .then( newAuthor => res.json(newAuthor))
-            .catch( err => res.status(400).json(err));
-    },
-
-    updateAuthor: (req, res) => {
-        Author.findByIdAndUpdate(req.params.id, req.body, {new: true})
-            .then(updatedAuthor => res.json(updatedAuthor))
-            .catch( err => res.status(400).json(err));
-    },
-
-    deleteAuthor : (req, res) => {
-        Author.findByIdAndDelete(req.params.id)
-            .then(deletedAuthor => res.json(deletedAuthor))
-            .catch( err => res.status(400).json(err));
-    }
+module.exports = app => {
+    app.get('/api/authors', authorController.findAllAuthors);
+    app.post('/api/auhors', authorController.createAuthor);
+    app.patch('/api/authors/:id', authorController.updateAuthor);
+    app.delete('/api/authors/:id', authorController.deleteAuthor);
 }
